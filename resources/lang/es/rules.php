a9db2378a46fd0b024780a40322cf48a7d309ef0
DiegoAvmor <diegoavmor@gmail.com>
2021-06-19 14:29:01 -0500

    Added validation result messages

--- resources/lang/es/rules.php
+++ resources/lang/es/rules.php
@@ -15,5 +15,8 @@ return [
     'exists' => 'El metadato :tag no existe.',
     'valid' => 'El metadato fue validado correctamente.',
     'title_format' => 'El formato del titulo no se apega al definido por la CONACYT (Título:Subtítulo).',
+    'access_level_format' => 'El nivel de acceso definido no se apega a los definidos en el sistema (closedAccess,embargoedAccess,restrictedAccess,openAccess).',
+    'licence_format' => 'El recurso no cuenta con una licencia valida.',
+    'date_format' => 'El formato de la fecha no se apega al de la norma ISO-8601'
 
 ];
