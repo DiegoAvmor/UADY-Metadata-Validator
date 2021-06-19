2b5fd419fb3d4a00df5864b28299aa24baa5179e
DiegoAvmor <diegoavmor@gmail.com>
2021-06-19 13:01:54 -0500

    Added spanish language translations

---
+++ resources/lang/es/rules.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mensajes de resultado de las reglas
+    |--------------------------------------------------------------------------
+    |
+    | las siguientes lineas de codigo representan los mensajes que se manejaran de acuerdo a las reglas correspondientes
+    | y los casos de validación que se aplique por el sistema
+    |
+    */
+
+    'exists' => 'El metadato :tag no existe.',
+    'valid' => 'El metadato fue validado correctamente.',
+    'title_format' => 'El formato del titulo no se apega al definido por la CONACYT (Título:Subtítulo).',
+
+];
