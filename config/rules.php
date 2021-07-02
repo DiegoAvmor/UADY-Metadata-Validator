abe14af83983edd186714b267d600c9601a9580a
DiegoAvmor <diegoavmor@gmail.com>
2021-07-02 13:29:22 -0500

    Updated rules format

--- config/rules.php
+++ config/rules.php
@@ -8,62 +8,75 @@ return [
         "Title" =>[
             'description' => "Valida la existencia del titulo en los metadatos",
             'instance' => Validators\TitleValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:title'
         ],
         "Autor" =>[
             'description' => "Valida la existencia del autor en los metadatos",
             'instance' => Validators\AuthorValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:creator'
         ],
         "Proyect Identifier" =>[
             'description' => "Valida la existencia del identificador del proyecto en los metadatos",
             'instance' => Validators\ProjectIdentifierValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:relation'
         ],
         "Access Level" =>[
             'description' => "Valida el nivel de acceso del recurso",
             'instance' => Validators\AccessLevelValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:rights'
         ],
         "License Condition" =>[
             'description' => "Valida la condición de licencia del recurso",
             'instance' => Validators\LicenseValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:rights'
         ],
         "Fecha de Publicación" =>[
             'description' => "Valida la condición de licencia del recurso",
             'instance' => Validators\DateValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:date'
         ],
         "Contribuidor" =>[
             'description' => "Valida la existencia de un contribuidor",
             'instance' => Validators\ContributorValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:contribuitor'
         ],
         "Tipo de Publicación" => [
             'description' => "Valida la existencia del tipo de publicación de un recurso",
             'instance' => Validators\PublicationTypeValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:type'
         ],
         "Language" => [
             'description' => "Valida la existencia del recurso lenguage",
             'instance' => Validators\LanguageValidator::getInstance(),
+            'ruleType' => 'M',
             'tag' => 'dc:language'
         ],
+        //Obligatorios cuando Aplique
         //Recomendados
         "Relation" => [
             'description' => 'Valida el formato del recurso relation en caso de que exista el tag',
             'instance' => Validators\RelationValidator::getInstance(),
+            'ruleType' => 'R',
             'tag' => 'dc:relation'
         ],
         "Coverage" => [
             'description' => 'Valida el formato del recurso coverage en caso de que exista el tag',
             'instance' => Validators\CoverageValidator::getInstance(),
+            'ruleType' => 'R',
             'tag' => 'dc:coverage'
         ],
         "Audience" => [
             'description' => 'Valida la existencia del recurso audience en caso de que exista el tag',
             'instance' => Validators\AudienceValidator::getInstance(),
+            'ruleType' => 'R',
             'tag' => 'dc:audience'
         ]
     ]
