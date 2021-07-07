e560eedccd281b17579d92fed16e62bdcc541ea5
davidita118 <david.isaac.iker@hotmail.com>
2021-07-07 01:19:48 -0500

    feat: implements XML format

--- app/Http/Controllers/ValidatorController.php
+++ app/Http/Controllers/ValidatorController.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 use App\Services\HarvesterService;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\Console\Input\Input;
+use App\Services\XMLService;
 
 class ValidatorController extends Controller
 {
     private $harvesterService;
+    private $XMLService;
 
     public function __construct(){
         $this->harvesterService = HarvesterService::getInstance();
+        $this->XMLService = XMLService::getInstance();
     }
 
     public function harvestURL(Request $request){
@@ -26,4 +29,14 @@ class ValidatorController extends Controller
         return view('welcome');
     }
 
+    public function formatXML(Request $request) {
+        try {
+            $xml = $request->input('---');
+            $this->XMLService->validateXML($xml);
+        } catch(Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+        return view('welcome');
+    }
 }
---
+++ app/Services/XMLService.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Services;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+use SimpleXMLElement;
+
+class XMLService extends ValidateService{
+    private static $instance = null;
+    
+    public static function getInstance(){
+        if (self::$instance == null){
+            self::$instance = new XMLService();
+        }
+        return self::$instance;
+    }
+
+    public function validateXML($xml) {
+
+        try {
+            $xmlObject = new SimpleXMLElement($xml);
+            $records = $xmlObject->ListRecords->record;
+
+            $validationResults = [];
+            foreach ($records as $record) {
+                $metadata = $record->metadata->children('oai_dc', 1)->dc->children('dc', 1);
+                $validationResults[] = $this->validateResource($metadata);
+            }
+            
+            dd($validationResults);
+            return $validationResults;
+        } catch(Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+    }
+}
+
+?>
