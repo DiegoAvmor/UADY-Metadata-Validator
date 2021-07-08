0ef0b1b4631090c438aa6b0496265ded4ae6fc73
GreenNugget <naomi.snchz99@gmail.com>
2021-07-07 23:56:00 -0500

    Added contributorId, publicationDate and version (type) and publisher

---
+++ app/Services/Validators/PublicationVersionValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Services\Validators;
+
+use Exception;
+use Illuminate\Support\Facades\Log;
+
+class PublicationVersionValidator extends RuleValidator
+{
+    private static $instance = null;
+
+    public static function getInstance()
+    {
+        if (self::$instance == null) {
+            self::$instance = new PublicationVersionValidator();
+        }
+        return self::$instance;
+    }
+
+    public function validateMetadata($content)
+    {
+        try {
+            $typeTag = $content->type;
+
+            if ($this->validateExistence($typeTag)) {
+                $matches = (bool) preg_match('(draft|submittedVersion|acceptedVersion|publishedVersion|updatedVersion)',(string) $typeTag);
+
+                return $this->buildValidationResponse($matches, $matches ? trans('rules.valid') : trans('rules.type'));
+            }
+            return $this->buildValidationResponse(false, trans('rules.exists', ['tag' => 'type']));
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+            return $this->buildValidationResponse(false, $exception->getMessage());
+        }
+    }
+}
