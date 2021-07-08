43c06e1b87aa7fb51f7aecba8646b20479c2c011
davidita118 <david.isaac.iker@hotmail.com>
2021-07-08 08:45:32 -0500

    feat: restructure services

--- app/Services/XMLService.php
+++ app/Services/XMLService.php
@@ -5,7 +5,7 @@ use Exception;
 use Illuminate\Support\Facades\Log;
 use SimpleXMLElement;
 
-class XMLService extends ValidateService{
+class XMLService extends ValidatorService{
     private static $instance = null;
     
     public static function getInstance(){
@@ -26,9 +26,8 @@ class XMLService extends ValidateService{
                 $metadata = $record->metadata->children('oai_dc', 1)->dc->children('dc', 1);
                 $validationResults[] = $this->validateResource($metadata);
             }
-            
-            dd($validationResults);
-            return $validationResults;
+
+            return $this->createQualityArray($validationResults);
         } catch(Exception $e) {
             Log::error($e->getMessage());
         }
